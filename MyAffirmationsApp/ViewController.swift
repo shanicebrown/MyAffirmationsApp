40d5a66dc27d76fdc103ab11abe33d58b7e41e5f
shanicebrown <sbrown30@csu.edu>
2020-07-19 16:48:59 -0500

    Initial Commit

---
+++ MyAffirmationsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAffirmationsApp
+//
+//  Created by KTG TECH on 7/19/20.
+//  Copyright © 2020 iShanicey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
